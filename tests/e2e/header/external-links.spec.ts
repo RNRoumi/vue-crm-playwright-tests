e07e29815262a06a1f7335ffa4bfa8ba12f60982
Roman <roman.kulikov@simbirsoft.com>
2025-08-14 15:17:50 +0300

    added new UI tests and a few utils

--- tests/e2e/header/external-links.spec.ts
+++ tests/e2e/header/external-links.spec.ts
@@ -6,7 +6,7 @@ test.describe('@header external links', () => {
     test.beforeEach(async ({adminPage}) => {
         await adminPage.gotoMainPage();
     })
-    test('GitHub открывается в новой вкладке', async ({adminPage, context}) => {
+    test('GitHub открывается в новой вкладке', async ({adminPage}) => {
         const popUpPage  = await adminPage.header.openGitHubPopUp();
         await popUpPage.waitForLoadState()
         await expect(popUpPage).toHaveURL(/github\.com/)
